29ffb7152f11a1b51e0a62c9df013ec3a197a5d7
wangronin <wangronin@gmail.com>
2021-12-05 20:47:49 +0100

    update

--- hvh/hypervolume_hessian.py
+++ hvh/hypervolume_hessian.py
@@ -1,11 +1,9 @@
-from typing import List, Tuple, Union
+from typing import Dict, List, Tuple, Union
 
 import numpy as np
+from scipy.linalg import block_diag
 
 from .hypervolume import hypervolume
-
-# from numba.core.decorators import njit
-
 
 __author__ = ["Hao Wang"]
 
@@ -32,29 +30,41 @@ def hypervolume_improvement(x: np.ndarray, pareto_front: np.ndarray, ref: np.nda
 
 
 class HypervolumeHessian:
-    """Analytical Hessian matrix of hypervolume"""
+    """Analytical Hessian matrix of hypervolume indicator"""
 
     def __init__(
         self,
-        pareto_front: Union[np.ndarray, List[List]],
+        dim_d: int,
+        dim_m: int,
         ref: Union[np.ndarray, List[List]],
+        func: callable = None,
+        jac: callable = None,
+        hessian: callable = None,
         maximization: bool = True,
     ):
         """Compute the hypervolume Hessian matrix
 
         Parameters
         ----------
-        pareto_front : Union[np.ndarray, List[List]]
-            the Pareto front
         ref : Union[np.ndarray, List[List]]
             the reference point
         maximization : bool, optional
             whether the MOP is subject to maximization, by default True
         """
+        if func is None:
+            func = lambda x: x
+        if jac is None:
+            jac = lambda x: np.diag(np.ones(len(x)))
+        if hessian is None:
+            hessian = lambda x: np.zeros((len(x), len(x), len(x)))
+
+        self.dim_d = int(dim_d)
+        self.dim_m = int(dim_m)
+        self.func = func
+        self.jac = jac
+        self.hessian = hessian
         self.maximization = maximization
-        self.pareto_front = pareto_front
         self.ref = ref
-        self.N, self.dim = self.pareto_front.shape
 
     @property
     def ref(self):
@@ -67,65 +77,87 @@ class HypervolumeHessian:
         self._ref = r if self.maximization else -1 * r
 
     @property
-    def pareto_front(self):
-        return self._pareto_front
+    def objective_points(self):
+        return self._objective_points
 
-    @pareto_front.setter
-    def pareto_front(self, pf):
-        if not isinstance(pf, np.ndarray):
-            pf = np.asarray(pf)
-        self._pareto_front = pf if self.maximization else -1 * pf
-        pf_indices = get_non_dominated(self._pareto_front, return_index=True)
-        self._dominated_indices = set(range(len(self._pareto_front))) - set(pf_indices)
+    @objective_points.setter
+    def objective_points(self, points):
+        if not isinstance(points, np.ndarray):
+            points = np.asarray(points)
+        self._objective_points = points if self.maximization else -1 * points
+        assert self.dim_m == self._objective_points.shape[1]
+        self._nondominated_indices = get_non_dominated(self._objective_points, return_index=True)
+        self._dominated_indices = set(range(len(self._objective_points))) - set(self._nondominated_indices)
 
     def project(
         self, axis: int, i: int, pareto_front: np.ndarray = None
     ) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
         """projecting the pareto front along `axis` with respect to the i-th point"""
-        pareto_front = self.pareto_front if pareto_front is None else pareto_front
-        x = pareto_front[i, :]
+        pareto_front = self.objective_points if pareto_front is None else pareto_front
+        y = pareto_front[i, :]
         # projection: drop the `axis`-th dimension
-        x_ = np.delete(x, obj=axis)
+        y_ = np.delete(y, obj=axis)
         ref_ = np.delete(self.ref, obj=axis)
-        idx = np.nonzero(self.pareto_front[:, axis] > x[axis])[0]
-        pareto_front_ = np.delete(self.pareto_front[idx, :], obj=axis, axis=1)
+        idx = np.nonzero(self.objective_points[:, axis] > y[axis])[0]
+        pareto_front_ = np.delete(self.objective_points[idx, :], obj=axis, axis=1)
         if len(pareto_front_) != 0:
             pareto_indices = get_non_dominated(pareto_front_, return_index=True)
             pareto_front_ = pareto_front_[pareto_indices]
             idx = idx[pareto_indices]
-        return x_, pareto_front_, ref_, idx
+        return y_, pareto_front_, ref_, idx
 
-    def compute(self) -> np.ndarray:
-        H = np.zeros((self.N * self.dim, self.N * self.dim))
+    def compute(self, X: Union[np.ndarray, List[List]]) -> Dict[str, np.ndarray]:
+        """compute the Hessian matrix
+
+        Parameters
+        ----------
+        X : Union[np.ndarray, List[List]]
+            the decision points at which the Hessian is computed
+
+        Returns
+        -------
+        Dict[str, np.ndarray]
+            {"HdX2": Hessian w.r.t. the decision variable of shape (`N` * `dim`, `N` * `dim`),
+             "HdY2": Hessian w.r.t. the decision variable of shape (`N` * `dim`, `N` * `dim`)}
+        """
+        Y, YdX, YdX2 = self._copmute_objective_derivatives(X)
+        self.objective_points = Y
+        HdX2 = np.zeros((self.N * self.dim_d, self.N * self.dim_d))
+        HdY2 = np.zeros((self.N * self.dim_m, self.N * self.dim_m))
+
         for i in range(self.N):
-            if i in self._dominated_indices:
+            if i in self._dominated_indices:  # if the point is dominated
                 continue
-            for k in range(self.dim):
-                # project along `axis`
-                x_, pareto_front_, ref_, proj_idx = self.project(k, i)
+            for k in range(self.dim_m):
+                # project along `axis`; `*_` indicates variables in the projected subspace
+                y_, pareto_front_, ref_, proj_idx = self.project(k, i)
                 # partial derivatives ∂(∂HV/∂y_k^i)/∂y^i
                 # of shape (1, dim), where the k-th element is zero
-                X = np.vstack([x_, pareto_front_])
-                pareto_indices = get_non_dominated(X, return_index=True)
+                Y_ = np.vstack([y_, pareto_front_])
+                pareto_indices = get_non_dominated(Y_, return_index=True)
                 idx = np.where(pareto_indices == 0)[0]
-                out = self.hypervolume_gradient(X[pareto_indices], ref_)[idx]
-                H[i * self.dim : (i + 1) * self.dim, i * self.dim + k] = np.insert(out, k, 0)
+                out = self.hypervolume_dY(Y_[pareto_indices], ref_)[idx]
+                HdY2[i * self.dim_m : (i + 1) * self.dim_m, i * self.dim_m + k] = np.insert(out, k, 0)
                 # partial derivatives ∂(∂HV/∂y_k^i)/∂y^{-i}
                 # of shape (len(proj_idx), dim), where the k-th element is zero
                 # ∂HV/∂y_k^i is the hypervolume improvement of `x_` w.r.t. `pareto_front_`
-                out = self.hypervolume_gradient(np.clip(pareto_front_, ref_, x_), ref=ref_)
+                out = self.hypervolume_dY(np.clip(pareto_front_, ref_, y_), ref=ref_)
                 # get the dimension of points in `pareto_front_` that are not dominated by `x_`
-                idx = pareto_front_ > x_
+                idx = pareto_front_ > y_
                 out[idx] = 0
                 # hypervolume improvement of points in `pareto_front_` decreases ∂HV/∂y_k^i
                 out = np.insert(-1.0 * out, k, 0, axis=1)
                 for s, j in enumerate(proj_idx):
-                    H[j * self.dim : (j + 1) * self.dim, i * self.dim + k] = out[s]
-        return H
+                    HdY2[j * self.dim_m : (j + 1) * self.dim_m, i * self.dim_m + k] = out[s]
 
-    def hypervolume_gradient(self, pareto_front: np.ndarray, ref: np.ndarray) -> np.ndarray:
+        HdY = self.hypervolume_dY(self.objective_points, self.ref).reshape(1, -1)
+        # TODO: use sparse matrix multiplication here
+        HdX2 = YdX.T @ HdY2 @ YdX + np.einsum("...i,i...", HdY, YdX2)
+        return dict(HdX2=HdX2, HdY2=HdY2)
+
+    def hypervolume_dY(self, pareto_front: np.ndarray, ref: np.ndarray) -> np.ndarray:
         if len(ref) == 2:  # 2D is a simple case
-            return self._2D_hypervolume_gradient(pareto_front, ref)
+            return self._2D_hypervolume_dY(pareto_front, ref)
         # general HV gradient in higher dimensions
         N, dim = pareto_front.shape
         grad = np.zeros((N, dim))
@@ -135,7 +167,7 @@ class HypervolumeHessian:
                 grad[i, k] = hypervolume_improvement(x_, pareto_front_, ref_)
         return grad
 
-    def _2D_hypervolume_gradient(self, pareto_front: np.ndarray, ref: np.ndarray) -> np.ndarray:
+    def _2D_hypervolume_dY(self, pareto_front: np.ndarray, ref: np.ndarray) -> np.ndarray:
         N = len(pareto_front)
         gradient = np.zeros((N, 2))
         # sort the pareto front with repsect to y1
@@ -147,3 +179,23 @@ class HypervolumeHessian:
         gradient[idx, 0] = y2 - np.r_[y2[1:], ref[1]]
         gradient[idx, 1] = y1 - np.r_[ref[0], y1[0:-1]]
         return gradient
+
+    def _copmute_objective_derivatives(
+        self, X: Union[np.ndarray, List[List]]
+    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+        if not isinstance(X, np.ndarray):
+            X = np.asarray(X)
+        if X.shape[1] != self.dim_d:
+            X = X.T
+        self.N = X.shape[0]  # number of points
+        Y = np.array([self.func(x) for x in X])  # `(N, dim_m)`
+        # Jacobians
+        YdX = np.array([self.jac(x) for x in X])  # `(N, dim_m, dim_d)`
+        YdX = block_diag(*YdX)  # `(N * dim_m, N * dim_d)`
+        # Hessians
+        _YdX2 = np.array([self.hessian(x) for x in X])  # `(N, dim_m, dim_d, dim_d)`
+        YdX2 = np.zeros((self.N * self.dim_m, self.N * self.dim_d, self.N * self.dim_d))
+        for i in range(self.N):
+            idx = slice(i * self.dim_d, (i + 1) * self.dim_d)
+            YdX2[i * self.dim_m : (i + 1) * self.dim_m, idx, idx] = _YdX2[i, ...]
+        return Y, YdX, YdX2
