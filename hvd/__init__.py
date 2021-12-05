db6d224af4ab2cc0bddf2f753b5962c027984758
wangronin <wangronin@gmail.com>
2021-12-05 21:03:56 +0100

    update

--- hvd/__init__.py
+++ hvd/__init__.py
@@ -1,3 +1,3 @@
-from .hypervolume_hessian import HypervolumeDerivatives, get_non_dominated
+from .hypervolume_derivatives import HypervolumeDerivatives, get_non_dominated
 
 __all__ = ["get_non_dominated", "HypervolumeDerivatives"]
