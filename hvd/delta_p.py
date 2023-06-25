5c95c9652fc57257e622cf7697cc26f1ac5f4e8c
wangronin <wangronin@gmail.com>
2023-06-25 13:21:41 +0200

    fitst implemenation of IGDN

--- hvd/delta_p.py
+++ hvd/delta_p.py
@@ -96,6 +96,90 @@ class GenerationalDistance:
             return grad
 
 
-# TODO:
 class InvertedGenerationalDistance:
-    pass
+    def __init__(self, ref: np.ndarray, func: Callable, jac: Callable, hess: Callable, p: float = 2):
+        """Generational Distance
+
+        Args:
+            ref (np.ndarray): the reference set, of shape (n_point, n_objective)
+            func (Callable): the objective function, which returns an array of shape (n_objective, )
+            jac (Callable): the Jacobian function, which returns an array of shape (n_objective, dim)
+            hess (Callable): the Hessian function, which returns an array of shape (n_objective, dim, dim)
+            p (float, optional): parameter in the p-norm. Defaults to 2.
+        """
+        self.ref = ref
+        self.p = p
+        self.func = func
+        self.jac = jac
+        self.hess = hess
+        self.M = len(self.ref)
+
+    def _compute_indices(self, Y: np.ndarray):
+        """find for each reference point, the index of its closest point in the approximation set
+
+        Args:
+            Y (np.ndarray): the objective points of shape (N, n_objective).
+        """
+        N = len(Y)
+        self.D = cdist(Y, self.ref, metric="minkowski", p=self.p)
+        # for each point `p`` in `Y`, the indices of points in the reference set
+        # which have `p` as the closest point.
+        self._indices = np.argmin(self.D, axis=0)
+        self.indices = [np.nonzero(self._indices == i)[0] for i in range(N)]
+        # for each point `p` in `Y`, the total number of points in the reference set
+        # which have `p` as the closest point
+        self.m = np.array([len(idx) for idx in self.indices]).reshape(-1, 1)
+
+    def compute(self, X: np.ndarray = None, Y: np.ndarray = None) -> float:
+        """compute the inverted generational distance value
+
+        Args:
+            X (np.ndarray, optional): the decision points of shape (N, dim). Defaults to None.
+            Y (np.ndarray, optional): the objective points of shape (N, n_objective). Defaults to None.
+                `X` and `Y` cannot be None at the same time
+
+        Returns:
+            float: the indicator value
+        """
+        if Y is None:
+            assert X is not None
+            Y = np.array([self.func(x) for x in X])
+        self._compute_indices(Y)
+        return np.mean(self.D[self._indices, np.arange(self.M)] ** self.p) ** (1 / self.p)
+
+    def compute_derivatives(
+        self, X: np.ndarray, Y: np.ndarray = None, compute_hessian: bool = True
+    ) -> Union[np.ndarray, Tuple[np.ndarray, np.ndarray]]:
+        """compute the derivatives of the inverted generational distance^p
+
+        Args:
+            X (np.ndarray): the decision points of shape (N, dim).
+            Y (np.ndarray, optional): the objective points of shape (N, n_objective). Defaults to None.
+            compute_hessian (bool, optional): whether the Hessian is computed. Defaults to True.
+
+        Returns:
+            Union[np.ndarray, Tuple[np.ndarray, np.ndarray]]:
+                if `compute_hessian` = True, it returns (gradient, Hessian)
+                otherwise, it returns (gradient, )
+        """
+        # TODO: implement p != 2
+        c = 2 / self.M
+        dim = X.shape[1]
+        if Y is None:
+            Y = np.array([self.func(x) for x in X])
+
+        self._compute_indices(Y)
+        J = np.array([self.jac(x) for x in X])  # (N, n_objective, dim)
+        centroid = np.array([np.sum(self.ref[idx], axis=0) for idx in self.indices])
+        diff = self.m * Y - centroid  # (N, n_objective)
+        grad = c * np.einsum("ijk,ij->ik", J, diff)  # (N, dim)
+
+        if compute_hessian:
+            H = np.array([self.hess(x) for x in X])  # (N, n_objective, dim, dim)
+            m = np.tile(self.m[..., np.newaxis], (1, dim, dim))
+            hessian = c * (
+                m * np.einsum("ijk,ijl->ikl", J, J) + np.einsum("ijkl,ij->ikl", H, diff)
+            )  # (N, dim, dim)
+            return grad, hessian
+        else:
+            return grad
