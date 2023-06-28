0c98f1167a4a4a3c807cba865e99aac70609cdd2
wangronin <wangronin@gmail.com>
2023-06-28 12:31:01 +0200

    fix bugs

--- examples/DpN/3D_example2.py
+++ examples/DpN/3D_example2.py
@@ -2,16 +2,31 @@ import matplotlib.pyplot as plt
 import matplotlib.tri as mtri
 import numpy as np
 import pandas as pd
+from matplotlib import rcParams
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
-from hvd.newton import DeltapNewton
+from hvd.newton import DpN
 
 np.random.seed(42)
 np.set_printoptions(edgeitems=30, linewidth=100000)
-plt.style.use("ggplot")
 
-c1 = np.array([1, 1, 0])
-c2 = np.array([1, -1, 0])
-c3 = np.array([-1, 0, 0])
+plt.style.use("ggplot")
+rcParams["font.size"] = 12
+rcParams["xtick.direction"] = "out"
+rcParams["ytick.direction"] = "out"
+rcParams["text.usetex"] = True
+rcParams["legend.numpoints"] = 1
+rcParams["xtick.labelsize"] = 12
+rcParams["ytick.labelsize"] = 12
+rcParams["xtick.major.size"] = 7
+rcParams["xtick.major.width"] = 1
+rcParams["ytick.major.size"] = 7
+rcParams["ytick.major.width"] = 1
+
+
+c1 = np.array([-1, -1, -1])
+c2 = np.array([-1, 0, 0])
+c3 = np.array([-2, -2, 4])
 
 
 def MOP1(x):
@@ -27,7 +42,13 @@ def MOP1(x):
 
 def MOP1_Jacobian(x):
     x = np.array(x)
-    return np.array([2 * (x - c1), 2 * (x - c2), 2 * (x - c3)])
+    return np.array(
+        [
+            2 * (x - c1),
+            2 * (x - c2),
+            2 * (x - c3),
+        ]
+    )
 
 
 def MOP1_Hessian(x):
@@ -35,61 +56,67 @@ def MOP1_Hessian(x):
     return np.array([2 * np.eye(3), 2 * np.eye(3), 2 * np.eye(3)])
 
 
-center = np.array([2 / np.sqrt(3) - 1, 0, -1.5])
+def g(x):
+    return -1.0 * x[0]
 
 
-def h(x):
-    x = np.array(x)
-    return np.sum((x - center) ** 2) - 1
+def g_Jacobian(x):
+    v = np.zeros(len(x))
+    v[0] = -1
+    return v
 
 
-def h_Jacobian(x):
-    x = np.array(x)
-    return 2 * (x - center)
-
-
-N = 200
-point_set = []
-for i, x in enumerate(np.linspace(-1, 1, int(np.sqrt(N)))):
-    n = 1 + 2 * i
-    y_ = -0.5 * x - 0.5
-    _y = 0.5 * x + 0.5
+pareto_set = np.array([[0, -1, -1], [0, 0, 0], [0, -2, 4]])
+point_set = [np.array([[0, -1, -1], [0, 0, 0], [0, -2, 4]])]
+for z in np.linspace(-1.01, 3.99, 50):
+    _y = z / -2 if z >= 0 else z
+    y_ = (z + 6) / -5
+    N = int(10 * (_y - y_))
     point_set.append(
-        np.stack(
-            [
-                np.array([x] * n),
-                np.linspace(y_, _y, n),
-                np.array([0] * n),
-            ]
-        ).T
+        np.stack([np.array([0.1] * N), np.linspace(y_ - 0.01, _y + 0.01, N), np.array([z] * N)]).T
     )
 point_set = np.concatenate(point_set, axis=0)
-point_set -= center
-pareto_set = 0.98 * point_set / np.linalg.norm(point_set, axis=1).reshape(-1, 1) + center
-pareto_front = np.array([MOP1(x) for x in pareto_set])
+pareto_front = np.array([MOP1(x) for x in point_set])
 
-max_iters = 20
+dim = 3
+max_iters = 15
+
+# x0 = np.array(
+#     [
+#         [0.7, -1.8, -1],
+#         [0.65, 0, -1.2],
+#         [1.01, -2, 0.5],
+#         [1, 0.5, 1],
+#         [0.7, 1.5, -1.5],
+#         [1.2, 1.5, 2],
+#     ]
+# )
 mu = 50
-w = np.abs(np.random.rand(mu, 3))
+a = np.mgrid[-2.5:-0.5:5j, 0:3.5:10j]
+a = np.array(list(zip(a[0].ravel(), a[1].ravel())))
+w = np.abs(np.random.randn(mu, 3))
 w /= np.sum(w, axis=1).reshape(-1, 1)
-x0 = w @ np.vstack([c1, c2, c3])
-# x0[0, 2] = 1
-x0[:, 2] = 1.5
-y0 = np.array([MOP1(_) for _ in x0])
+# x0 = w @ np.vstack([c1, c2, c3])
+# x0[:, 0] = 0.5
+# x0[0, 0] = 3.5
 
-opt = DeltapNewton(
-    dim=3,
+x0 = np.c_[np.tile(1.5, (len(a), 1)), a]
+y0 = np.array([MOP1(_) for _ in x0])
+mu = len(x0)
+
+opt = DpN(
+    dim=dim,
     n_objective=3,
     ref=pareto_front,
     func=MOP1,
     jac=MOP1_Jacobian,
     hessian=MOP1_Hessian,
-    h=h,
-    h_jac=h_Jacobian,
-    mu=mu,
+    g=g,
+    g_jac=g_Jacobian,
+    mu=len(x0),
     x0=x0,
-    lower_bounds=-2,
-    upper_bounds=2,
+    lower_bounds=-4,
+    upper_bounds=4,
     minimization=True,
     max_iters=max_iters,
     type="deltap",
@@ -97,35 +124,28 @@ opt = DeltapNewton(
 )
 X, Y, stop = opt.run()
 
-fig = plt.figure(figsize=plt.figaspect(1 / 3.0))
+fig = plt.figure(figsize=plt.figaspect(1 / 3))
 ax = fig.add_subplot(1, 3, 1, projection="3d")
 ax.set_box_aspect((1, 1, 1))
-ax.view_init(50, -25)
+ax.view_init(25, -55)
 
-u, v = np.mgrid[0 : 2 * np.pi : 18j, 0 : np.pi : 16j]
-r = 1
-x = r * np.cos(u) * np.sin(v)
-y = r * np.sin(u) * np.sin(v)
-z = r * np.cos(v)
-x += 2 / np.sqrt(3) - 1
-z -= 1.5
-ax.plot_wireframe(x, y, z, alpha=0.3, color="r")
-
-x, y, z = pareto_set[:, 0], pareto_set[:, 1], pareto_set[:, 2]
-triang = mtri.Triangulation(x, y)
-ax.plot_trisurf(triang, z, color="k", alpha=0.3)
-
-# plot the initial decision points
-ax.plot(x0[:, 0], x0[:, 1], x0[:, 2], "g.", ms=8)
+# plot the constraint boundary
+yy, zz = np.mgrid[-2.5:2:5j, -1.5:4.5:5j]
+xx = np.zeros(yy.shape)
+ax.plot_surface(xx, yy, zz, alpha=0.2)
+# plot the efficient set
+ax.add_collection3d(Poly3DCollection([pareto_set], color="k", alpha=0.3))
+# plot the initial points
+ax.plot(x0[:, 0], x0[:, 1], x0[:, 2], "g.", ms=10)
 # plot the final decision points
 ax.plot(X[:, 0], X[:, 1], X[:, 2], "g*", ms=6)
+# ax.plot(point_set[:, 0], point_set[:, 1], point_set[:, 2], "r.", ms=8)
 ax.set_title("decision space")
 ax.set_xlabel(r"$x_1$")
 ax.set_ylabel(r"$x_2$")
 ax.set_zlabel(r"$x_3$")
-ax.set_xlim([-1.3, 1.3])
-ax.set_ylim([-1.3, 1.3])
-ax.set_zlim([-2.5, 0.3])
+ax.set_ylim([-2.5, 2])
+ax.set_xlim([-1.5, 1.5])
 
 trajectory = np.atleast_3d([x0] + opt.hist_X)
 for i in range(len(x0)):
@@ -138,13 +158,13 @@ for i in range(len(x0)):
         y[1:] - y[:-1],
         z[1:] - z[:-1],
         color="k",
-        arrow_length_ratio=0.1,
-        alpha=0.3,
+        arrow_length_ratio=0.08,
+        alpha=0.35,
     )
 
 ax = fig.add_subplot(1, 3, 2, projection="3d")
 ax.set_box_aspect((1, 1, 1))
-ax.view_init(-12, -135)
+ax.view_init(30, 13)
 
 # plot the reference set
 ax.plot(pareto_front[:, 0], pareto_front[:, 1], pareto_front[:, 2], "r.", ms=8)
@@ -159,12 +179,11 @@ p = np.c_[xmid, ymid, zmid]
 mask = np.array([np.any(np.all(pp > p, axis=1)) for pp in p])
 triang.set_mask(mask)
 
-# plot the Pareto front
-ax.plot_trisurf(triang, z, color="k", alpha=0.2)
-# plot the initial Pareton approximation set
-ax.plot(y0[:, 0], y0[:, 1], y0[:, 2], "g.", ms=8)
-# plot the final Pareton approximation set
+# ax.plot(x, y, z, "r.", ms=8)
 ax.plot(Y[:, 0], Y[:, 1], Y[:, 2], "g*", ms=8)
+# plot the initial points
+ax.plot(y0[:, 0], y0[:, 1], y0[:, 2], "g.", ms=10)
+ax.plot_trisurf(triang, z, color="k", alpha=0.2)
 
 trajectory = np.atleast_3d([y0] + opt.hist_Y)
 for i in range(len(x0)):
@@ -198,8 +217,7 @@ ax.legend()
 
 plt.tight_layout()
 plt.subplots_adjust(wspace=0.1)
-# plt.show()
-plt.savefig(f"3D-example1-{mu}.pdf", dpi=100)
+plt.savefig(f"3D-example2-{mu}.pdf", dpi=100)
 
 # df = pd.DataFrame(dict(iteration=range(1, len(opt.hist_HV) + 1), HV=opt.hist_HV, G_norm=opt.hist_G_norm))
-# df.to_latex(buf=f"3D-example1-{mu}.tex", index=False)
+# df.to_latex(buf=f"3D-example2-{mu}.tex", index=False)
