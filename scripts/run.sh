8bdc68733d905fe0787fa3f8fe8ca4dba69c6caf
wangronin <wangronin@gmail.com>
2023-10-26 14:54:31 +0200

    update

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,10 +7,10 @@
 #SBATCH --mail-user=h.wang@liacs.leidenuniv.nl
 #SBATCH --mail-type=END,FAIL
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=30
+#SBATCH --cpus-per-task=15
 #SBATCH --error="./err/HVI/%x-%j-%a.err"
 #SBATCH --output="./out/HVI/%x-%j-%a.out"
 
 PROBLEMS=(CF1 CF2 CF3 CF4 CF5 CF6 CF7 CF8 CF9)
 cd $HOME/HypervolumeDerivatives
-srun -N1 -n1 -c30 --exclusive python benchmark_hybrid_DpN.py &
+srun -N1 -n1 -c15 --exclusive python benchmark_hybrid_DpN.py &
