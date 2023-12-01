44dbf018a8ede2ff7ac53440297c737a2a6b0741
wangronin <wangronin@gmail.com>
2023-12-01 19:45:46 +0100

    update

---
+++ scripts/run_EA_save_population.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=DpN
+#SBATCH --array=0-21
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=15
+#SBATCH --partition=cpu-short
+#SBATCH --time=03:00:00
+#SBATCH --error="%x-%j-%a.err"
+#SBATCH --output="%x-%j-%a.out"
+#SBATCH --mail-type=END,FAIL
+
+source .bashrc
+cd $HOME/HypervolumeDerivatives/
+source venv/bin/activate
+export PYTHONPATH=./:$PYTHONPATH
+
+srun --ntasks=1 --cpus-per-task=15 python scripts/run_EA_save_population.py $SLURM_ARRAY_TASK_ID
