d7267a05c671c0f9e79ac053e7d183174488bbe3
wangronin <wangronin@gmail.com>
2023-12-11 22:30:08 +0100

    update

--- examples/DpN/ZDT_example.py
+++ examples/DpN/ZDT_example.py
@@ -6,7 +6,7 @@ import numpy as np
 from matplotlib import rcParams
 
 from hvd.newton import DpN
-from hvd.problems.zdt import ZDT1, ZDT2, ZDT3, ZDT4, ZDT6, PymooProblemWithAD
+from hvd.problems import ZDT6, PymooProblemWithAD
 
 plt.style.use("ggplot")
 rcParams["font.size"] = 17
@@ -28,14 +28,14 @@ np.random.seed(42)
 # ZDT3: stagnation in local efficient points, which is natural on this problem
 # ZDT6: indefinite DpN Hessians; Hessian modification method is needed
 
-N = 10
-max_iters = 5
+N = 5
+max_iters = 1
 problem = PymooProblemWithAD(ZDT6(n_var=2))
 pareto_front = problem.get_pareto_front(500)
 pareto_set = problem.get_pareto_set(500)
 
 ref = problem.get_pareto_front(100)
-ref -= 0.02
+ref -= 0.05
 # get the re-image of the reference set
 # f_x2 = (
 #     lambda x1, c: 0.111111111111111 * c
@@ -48,7 +48,7 @@ ref -= 0.02
 # ref_x = np.c_[x1, x2]
 # the initial approximation set
 x0 = problem.get_pareto_set(N, kind="uniform")
-x0[:, 1:] += 0.01 * np.random.rand(N, problem.n_var - 1)
+x0[:, 1:] += 0.0 * np.random.rand(N, problem.n_var - 1)
 y0 = np.array([problem.objective(x) for x in x0])
 
 fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(20, 6.5))
@@ -57,25 +57,13 @@ ax1.plot(y0[:, 0], y0[:, 1], "r.", ms=7, alpha=0.5)
 ax1.plot(pareto_front[:, 0], pareto_front[:, 1], "g.", mec="none", ms=5, alpha=0.35)
 ax1.plot(ref[:, 0], ref[:, 1], "b.", ms=4, mec="none", alpha=0.35)
 
-ax2.plot(x0[:, 0], x0[:, 1], "r.", ms=7, alpha=0.5)
-ax2.plot(pareto_set[:, 0], pareto_set[:, 1], "g.", mec="none", ms=5, alpha=0.4)
+ax2.plot(x0[:, 0], x0[:, 1], "r.", ms=9, alpha=0.8, clip_on=False)
+# ax2.plot(pareto_set[:, 0], pareto_set[:, 1], "g.", mec="none", ms=5, alpha=0.4)
 # ax2.plot(ref_x[:, 0], ref_x[:, 1], "b.", ms=4, mec="none", alpha=0.3)
 
-x1 = np.linspace(0, 1, 100)
-x2 = np.linspace(0, 0.1, 30)
-X1, X2 = np.meshgrid(x1, x2)
-Z = np.array([problem.objective(p) for p in np.array([X1.flatten(), X2.flatten()]).T])
-Z1 = Z[:, 0].reshape(len(x2), -1)
-Z2 = Z[:, 1].reshape(len(x2), -1)
-CS1 = ax2.contourf(X1, X2, Z1, 10, cmap=plt.cm.gray, linestyles="none", alpha=0.6)
-CS2 = ax2.contourf(X1, X2, Z2, 10, cmap=plt.cm.gray, linestyles="None", alpha=0.6)
+ax3.plot(x0[:, 0], x0[:, 1], "r.", ms=9, alpha=0.8, clip_on=False)
+# ax3.plot(pareto_set[:, 0], pareto_set[:, 1], "g.", mec="none", ms=5, alpha=0.4)
 
-ax1.set_title("Objective space")
-ax1.set_xlabel(r"$f_1$")
-ax1.set_ylabel(r"$f_2$")
-ax2.set_title("Decision space")
-ax2.set_xlabel(r"$x_1$")
-ax2.set_ylabel(r"$x_2$")
 
 opt = DpN(
     dim=problem.n_var,
@@ -84,8 +72,8 @@ opt = DpN(
     func=problem.objective,
     jac=problem.objective_jacobian,
     hessian=problem.objective_hessian,
-    g=problem.ieq_constraint,
-    g_jac=problem.ieq_jacobian,
+    # g=problem.ieq_constraint,
+    # g_jac=problem.ieq_jacobian,
     N=N,
     x0=x0,
     xl=problem.xl,
@@ -93,16 +81,118 @@ opt = DpN(
     max_iters=max_iters,
     type="igd",
     verbose=True,
+    pareto_front=pareto_front,
 )
 
-while not opt.terminate():
-    opt.reference_set = ref
-    opt.newton_iteration()
-    opt.log()
+X, Y, _ = opt.run()
+grad = opt.grad
+grad /= np.linalg.norm(grad, axis=1).reshape(-1, 1)
+grad *= -1
 
-X = opt._get_primal_dual(opt.X)[0]
-Y = opt.Y
-M = opt.active_indicator._medoids_cluster
+yl = -1e-5
+yu = 1e-4
+scale = yu - yl
+x1 = np.linspace(0, 1, 100)
+x2 = np.linspace(yl, yu, 100)
+X1, X2 = np.meshgrid(x1, x2)
+# Z = np.array([opt.active_indicator.compute(X=p) for p in np.array([X1.flatten(), X2.flatten()]).T])
+# Z = Z.reshape(len(x2), -1)
+Z = np.array([problem.objective(p) for p in np.array([X1.flatten(), X2.flatten()]).T])
+Z1 = Z[:, 0].reshape(len(x2), -1)
+Z2 = Z[:, 1].reshape(len(x2), -1)
+CS1 = ax2.contourf(X1, X2, Z1, 10, cmap=plt.cm.gray, linestyles="--", alpha=0.6)
+
+fig.colorbar(CS1, ax=ax2)
+CS2 = ax3.contourf(X1, X2, Z2, 20, cmap=plt.cm.gray, linestyles="--", alpha=0.6)
+fig.colorbar(CS2, ax=ax3)
+
+grad[:, 1] *= scale * 0.2
+grad[:, 0] *= 0.2
+
+for i in range(N):
+    ax3.quiver(
+        x0[i, 0],
+        x0[i, 1],
+        grad[i, 0],
+        grad[i, 1],
+        scale_units="xy",
+        angles="xy",
+        scale=1,
+        color="c",
+        width=0.004,
+        alpha=0.8,
+        headlength=4.7,
+        headwidth=2.7,
+        clip_on=False,
+    )
+    ax3.quiver(
+        x0[i, 0],
+        x0[i, 1],
+        opt.step[i, 0],
+        opt.step[i, 1],
+        scale_units="xy",
+        angles="xy",
+        scale=1,
+        color="k",
+        width=0.004,
+        alpha=0.8,
+        headlength=4.7,
+        headwidth=2.7,
+        clip_on=False,
+    )
+
+    diff = opt.V[i]
+    diff[1, :] *= scale * 0.5
+    diff[0, :] *= 0.5
+    color = "r" if opt.w[i][0] > 0 else "b"
+    ax2.quiver(
+        x0[i, 0],
+        x0[i, 1],
+        diff[0, 0],
+        diff[1, 0],
+        scale_units="xy",
+        angles="xy",
+        scale=1,
+        color=color,
+        width=0.004,
+        alpha=0.8,
+        headlength=4.7,
+        headwidth=2.7,
+        clip_on=False,
+    )
+    color = "r" if opt.w[i][1] > 0 else "b"
+    ax2.quiver(
+        x0[i, 0],
+        x0[i, 1],
+        diff[0, 1],
+        diff[1, 1],
+        scale_units="xy",
+        angles="xy",
+        scale=1,
+        color=color,
+        width=0.004,
+        alpha=0.8,
+        headlength=4.7,
+        headwidth=2.7,
+        clip_on=False,
+    )
+
+ax1.set_title("Objective space")
+ax1.set_xlabel(r"$f_1$")
+ax1.set_ylabel(r"$f_2$")
+ax2.set_title("Decision space")
+ax2.set_xlabel(r"$x_1$")
+ax2.set_ylabel(r"$x_2$")
+
+ax3.set_title("Decision space")
+ax3.set_xlabel(r"$x_1$")
+ax3.set_ylabel(r"$x_2$")
+
+ax2.set_ylim([-1e-5, 1e-4])
+ax3.set_ylim([-1e-5, 1e-4])
+
+
+M = np.vstack([m[-1] for m in opt.history_medoids])
 # x1 = M[:, 0]
 # x2 = [f_x2(*_) for _ in zip(x1, M[:, 1])]
 # M_x = np.c_[x1, x2]
@@ -111,6 +201,7 @@ ax1.plot(M[:, 0], M[:, 1], "r^", ms=7, alpha=0.5, clip_on=False)
 # ax2.plot(M_x[:, 0], M_x[:, 1], "r^", ms=7, alpha=0.5)
 ax1.plot(Y[:, 0], Y[:, 1], "r*", ms=7, alpha=0.5, clip_on=False)
 ax2.plot(X[:, 0], X[:, 1], "r*", ms=7, alpha=0.5, clip_on=False)
+# ax3.plot(X[:, 0], X[:, 1], "r*", ms=7, alpha=0.5, clip_on=False)
 # plot the trajectory
 trajectory = np.array([x0] + opt.hist_X)
 for i in range(N):
@@ -147,13 +238,13 @@ for i in range(N):
         headlength=4.7,
         headwidth=2.7,
     )
-ax3_ = ax3.twinx()
-ax3.semilogy(range(1, len(opt.hist_GD) + 1), opt.hist_GD, "b-", label="GD")
-ax3.semilogy(range(1, len(opt.hist_IGD) + 1), opt.hist_IGD, "r-", label="IGD")
-ax3_.semilogy(range(1, len(opt.hist_R_norm) + 1), opt.hist_R_norm, "g--")
-ax3_.set_ylabel(r"$||R(\mathbf{X})||$", color="g")
-ax3.set_title("Performance")
-ax3.set_xlabel("iteration")
-ax3.set_xticks(range(1, max_iters + 1))
-ax3.legend()
+# ax3_ = ax3.twinx()
+# ax3.semilogy(range(1, len(opt.hist_GD) + 1), opt.hist_GD, "b-", label="GD")
+# ax3.semilogy(range(1, len(opt.hist_IGD) + 1), opt.hist_IGD, "r-", label="IGD")
+# ax3_.semilogy(range(1, len(opt.hist_R_norm) + 1), opt.hist_R_norm, "g--")
+# ax3_.set_ylabel(r"$||R(\mathbf{X})||$", color="g")
+# ax3.set_title("Performance")
+# ax3.set_xlabel("iteration")
+# ax3.set_xticks(range(1, max_iters + 1))
+# ax3.legend()
 plt.savefig(f"{problem._problem.__class__.__name__}-2D-decision_space.pdf", dpi=1000)
--- scripts/example_ZDT6.py
+++ scripts/example_ZDT6.py
@@ -7,7 +7,7 @@ import pandas as pd
 from matplotlib import rcParams
 
 from hvd.newton import DpN
-from hvd.problems import ZDT3, PymooProblemWithAD
+from hvd.problems import ZDT6, PymooProblemWithAD
 
 plt.style.use("ggplot")
 rcParams["font.size"] = 17
@@ -26,26 +26,38 @@ np.random.seed(66)
 
 max_iters = 8
 run = 1
-f = ZDT3()
+f = ZDT6()
 problem = PymooProblemWithAD(f)
 pareto_front = problem.get_pareto_front(500)
 
 # load the reference set
-path = "./ZDT/ZDT3/"
-ref_label = pd.read_csv(path + f"ZDT3_NSGA-II_run_{run}_component_id.csv", header=None).values[0]
+path = "./Gen1510/"
+emoa = "NSGA-II"
+gen = 110
+problem_name = "ZDT6"
+ref_label = pd.read_csv(
+    f"{path}/{problem_name}_{emoa}_run_{run}_component_id_gen{gen}.csv", header=None
+).values[0]
 n_cluster = len(np.unique(ref_label))
 ref = dict()
 eta = dict()
+# load the reference set
 for i in range(n_cluster):
-    ref[i] = pd.read_csv(path + f"ZDT3_NSGA-II_run_{run}_filling_comp{i+1}.csv", header=None).values
-    eta[i] = pd.read_csv(path + f"ZDT3_NSGA-II_run_{run}_eta_{i+1}.csv", header=None).values.ravel()
-
+    ref[i] = pd.read_csv(
+        f"{path}/{problem_name}_{emoa}_run_{run}_filling_comp{i+1}_gen{gen}.csv", header=None
+    ).values
+    eta[i] = pd.read_csv(
+        f"{path}/{problem_name}_{emoa}_run_{run}_eta_{i+1}_gen{gen}.csv", header=None
+    ).values.ravel()
 all_ref = np.concatenate([v for v in ref.values()], axis=0)
+
 # the load the final population from an EMOA
-x0 = pd.read_csv(path + f"ZDT3_NSGA-II_run_{run}_lastpopu_x.csv", header=None).values[0:50]
-y0 = pd.read_csv(path + f"ZDT3_NSGA-II_run_{run}_lastpopu_y.csv", header=None).values[0:50]
-Y_label = pd.read_csv(path + f"ZDT3_NSGA-II_run_{run}_lastpopu_labels.csv", header=None).values.ravel()[0:50]
-Y_label -= 1
+x0 = pd.read_csv(f"{path}/{problem_name}_{emoa}_run_{run}_lastpopu_x_gen{gen}.csv", header=None).values
+y0 = pd.read_csv(f"{path}/{problem_name}_{emoa}_run_{run}_lastpopu_y_gen{gen}.csv", header=None).values
+Y_label = pd.read_csv(
+    f"{path}/{problem_name}_{emoa}_run_{run}_lastpopu_labels_gen{gen}.csv", header=None
+).values.ravel()
+Y_label = Y_label - 1
 N = len(x0)
 
 opt = DpN(
@@ -57,7 +69,7 @@ opt = DpN(
     hessian=problem.objective_hessian,
     g=problem.ieq_constraint,
     g_jac=problem.ieq_jacobian,
-    N=N,
+    N=len(x0),
     x0=x0,
     xl=problem.xl,
     xu=problem.xu,
@@ -68,10 +80,8 @@ opt = DpN(
     eta=eta,
     Y_label=Y_label,
 )
-opt.run()
+X, Y, _ = opt.run()
 medoids0 = np.vstack([m[0] for m in opt.history_medoids])
-X = opt._get_primal_dual(opt.X)[0]
-Y = opt.Y
 
 fig, (ax0, ax1, ax2) = plt.subplots(1, 3, figsize=(20, 6.5))
 plt.subplots_adjust(right=0.93, left=0.05)
