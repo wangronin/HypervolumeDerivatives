18c8a205beea1dd0607a1e9e703d8972cc4cb7c8
wangronin <wangronin@gmail.com>
2024-07-04 15:38:18 +0200

    update

--- examples/HVN/convex_example.py
+++ examples/HVN/convex_example.py
@@ -3,19 +3,20 @@ import sys
 sys.path.insert(0, "./")
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 from matplotlib import rcParams
 
 from hvd.newton import HVN
 
 plt.style.use("ggplot")
-rcParams["font.size"] = 17
+plt.rc("text.latex", preamble=r"\usepackage{amsmath}")
+rcParams["text.usetex"] = True
+rcParams["font.size"] = 13
 rcParams["xtick.direction"] = "out"
 rcParams["ytick.direction"] = "out"
 rcParams["text.usetex"] = True
 rcParams["legend.numpoints"] = 1
-rcParams["xtick.labelsize"] = 17
-rcParams["ytick.labelsize"] = 17
+rcParams["xtick.labelsize"] = 13
+rcParams["ytick.labelsize"] = 13
 rcParams["xtick.major.size"] = 7
 rcParams["xtick.major.width"] = 1
 rcParams["ytick.major.size"] = 7
@@ -37,36 +38,22 @@ def convex_Hessian(x):
 
 
 def g(x):
-    v = x[0] ** 2 - 2 * x[0] + 1
-    if v >= 1e-4:
-        return 0
-    else:
-        return v
+    return x[0] ** 2 - 2 * x[0] + 1 - x[1]
 
 
 def g_Jacobian(x):
-    v = x[0] ** 2 - 2 * x[0] + 1
-    if v >= 1e-4:
-        return np.array([0, 0])
-    else:
-        return np.r_[2 * x[0] - 2, 0]
+    return np.array([2 * x[0] - 2, -1])
 
 
 def g_Hessian(x):
-    v = x[0] ** 2 - 2 * x[0] + 1
-    if v >= 1e-4:
-        return np.zeros((2, 2))
-    else:
-        h = np.zeros((2, 2))
-        h[0, 0] = 2
-        return h
+    return np.array([[2.0, 0], [0, 0]])
 
 
 ref = np.array([1, 1])
-max_iters = 3
+max_iters = 10
 
 N = 10
-x1 = np.linspace(0, 1, N)
+x1 = np.random.rand(N)
 x2 = x1**2 - 2 * x1 + 1
 x0 = np.c_[x1, x2]
 y0 = x0.copy()
@@ -91,30 +78,30 @@ opt = HVN(
 )
 X, Y, stop = opt.run()
 
-fig, (ax0, ax1) = plt.subplots(1, 2, figsize=(18, 6.5))
+fig, (ax0, ax1) = plt.subplots(1, 2, figsize=(13, 6.5))
 plt.subplots_adjust(right=0.93, left=0.05)
-ciricle = plt.Circle((0, 0), 1, color="r", fill=False, ls="--", lw=1.5)
-
-ax0.plot(Y[:, 0], Y[:, 1], "g*")
-ax0.plot(y0[:, 0], y0[:, 1], "g.", ms=8)
+ax0.set_aspect("equal")
+ax0.plot(Y[:, 0], Y[:, 1], "r*")
+ax0.plot(y0[:, 0], y0[:, 1], "k+", ms=8)
 trajectory = np.array([y0] + opt.hist_Y)
 
-for i in range(N):
-    x, y = trajectory[:, i, 0], trajectory[:, i, 1]
-    ax0.quiver(
-        x[:-1],
-        y[:-1],
-        x[1:] - x[:-1],
-        y[1:] - y[:-1],
-        scale_units="xy",
-        angles="xy",
-        scale=1,
-        color="k",
-        width=0.005,
-        alpha=0.5,
-        headlength=4.7,
-        headwidth=2.7,
-    )
+if 11 < 2:
+    for i in range(N):
+        x, y = trajectory[:, i, 0], trajectory[:, i, 1]
+        ax0.quiver(
+            x[:-1],
+            y[:-1],
+            x[1:] - x[:-1],
+            y[1:] - y[:-1],
+            scale_units="xy",
+            angles="xy",
+            scale=1,
+            color="k",
+            width=0.005,
+            alpha=0.5,
+            headlength=4.7,
+            headwidth=2.7,
+        )
 
 x_vals = np.linspace(0, 1, 100)
 y_vals = x_vals**2 - 2 * x_vals + 1
@@ -122,14 +109,14 @@ ax0.plot(x_vals, y_vals, "r--")
 ax0.set_title("Objective space")
 ax0.set_xlabel(r"$f_1$")
 ax0.set_ylabel(r"$f_2$")
+ax0.legend([r"$Y_{\text{final}}$", r"$Y_0$", "Pareto front"])
 
-ax22 = ax1.twinx()
+ax12 = ax1.twinx()
 ax1.plot(range(1, len(opt.hist_HV) + 1), opt.hist_HV, "b-")
-ax22.semilogy(range(1, len(opt.hist_HV) + 1), opt.hist_G_norm, "g--")
+ax12.semilogy(range(1, len(opt.hist_HV) + 1), opt.hist_G_norm, "g--")
 ax1.set_ylabel("HV", color="b")
-ax22.set_ylabel(r"$||R(\mathbf{X})||$", color="g")
+ax12.set_ylabel(r"$||G(\mathbf{X})||$", color="g")
 ax1.set_title("Performance")
 ax1.set_xlabel("iteration")
 ax1.set_xticks(range(1, max_iters + 1))
-
 plt.savefig(f"convex-example-{N}.pdf", dpi=1000)
