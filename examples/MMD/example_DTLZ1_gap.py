c8ea7dd33d3411e3c0e50224db8faa191ec276a0
wangronin <wangronin@gmail.com>
2024-07-22 19:51:00 +0200

    add more examples of MMD on DTLZ1

--- examples/MMD/example_DTLZ1_gap.py
+++ examples/MMD/example_DTLZ1_gap.py
@@ -2,6 +2,8 @@ import sys
 
 sys.path.insert(0, "./")
 
+import random
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -27,21 +29,51 @@ rcParams["xtick.major.width"] = 1
 rcParams["ytick.major.size"] = 7
 rcParams["ytick.major.width"] = 1
 
+random.seed(66)
 np.random.seed(66)
-max_iters = 16
+max_iters = 20
 
 f = DTLZ1(boundry_constraints=True)
 problem = PymooProblemWithAD(f)
 pareto_front = problem.get_pareto_front()
 
-# read the reference set data
-ref_ = pd.read_csv("./DTLZ1/DTLZ1_RANDOM_run_1_ref_1_gen0.csv", header=None).values
-X0 = pd.read_csv("./DTLZ1/DTLZ1_RANDOM_run_1_lastpopu_x_gen0.csv", header=None).values
-Y0 = pd.read_csv("./DTLZ1/DTLZ1_RANDOM_run_1_lastpopu_y_gen0.csv", header=None).values
-eta = {0: pd.read_csv("./DTLZ1/DTLZ1_RANDOM_run_1_eta_1_gen0.csv", header=None).values.ravel()}
-Y_idx = None
-Y_label = np.array([0] * len(ref_))
-ref = {0: ref_}
+ref_ = pd.read_csv("./DTLZ1/DTLZ1_RANDOM_HOLE_run_1_ref_1_gen0.csv", header=None).values
+X0 = pd.read_csv("./DTLZ1/DTLZ1_RANDOM_HOLE_run_1_lastpopu_x_gen0.csv", header=None).values
+Y0 = pd.read_csv("./DTLZ1/DTLZ1_RANDOM_HOLE_run_1_lastpopu_y_gen0.csv", header=None).values
+
+idx = Y0[:, 2] >= 0.307
+X_component1 = X0[idx]
+Y_component1 = Y0[idx]
+ref1_ = pareto_front[pareto_front[:, 2] >= 0.307]
+idx = random.sample(range(len(ref1_)), len(Y_component1))
+ref1_ = ref1_[idx]
+
+idx = Y0[:, 2] <= 0.2
+X_component2 = X0[idx]
+Y_component2 = Y0[idx]
+ref2_ = pareto_front[pareto_front[:, 2] <= 0.2]
+idx = random.sample(range(len(ref2_)), len(Y_component2))
+ref2_ = ref2_[idx]
+
+if 1 < 2:
+    # NOTE: for bootstrapping, the spread looks better
+    # if we used a clustered reference set to represent the gap
+    ref = {0: ref1_, 1: ref2_}
+    eta = {0: -1 * np.array([1 / np.sqrt(3)] * 3), 1: -1 * np.array([1 / np.sqrt(3)] * 3)}
+    Y_idx = [list(range(0, len(Y_component1))), list(range(len(Y_component1), len(Y0)))]
+    Y_label = np.array([0] * len(X_component1) + [1] * len(X_component2))
+else:
+    X0 = X_component2
+    Y0 = Y_component2
+    ref = ref2_
+
+    # X0 = X_component1
+    # Y0 = Y_component1
+    # ref = ref1_
+
+    eta = {0: -1 * np.array([1 / np.sqrt(3)] * 3)}
+    Y_idx = None
+    Y_label = np.array([0] * len(X_component1))
 
 N = len(X0)
 metrics = dict(GD=GenerationalDistance(pareto_front), IGD=InvertedGenerationalDistance(pareto_front))
@@ -65,8 +97,8 @@ opt = MMDNewton(
     preconditioning=False,
 )
 if 1 < 2:
-    opt.indicator.beta = 0.25  # start with a large spreading effect
-    X, Y, _ = bootstrap_reference_set(opt, problem, 7)
+    opt.indicator.beta = 0.5  # start with a large spreading effect
+    X, Y, _ = bootstrap_reference_set(opt, problem, 10)
 else:
     X, Y, _ = opt.run()
 
@@ -106,7 +138,8 @@ ax0 = fig.add_subplot(1, 3, 1, projection="3d")
 ax0.set_box_aspect((1, 1, 1))
 ax0.view_init(45, 45)
 ax0.plot(Y0[:, 0], Y0[:, 1], Y0[:, 2], "r+", ms=8, alpha=0.6)
-ax0.plot(ref_[:, 0], ref_[:, 1], ref_[:, 2], "g.", ms=6, alpha=0.6)
+ax0.plot(ref1_[:, 0], ref1_[:, 1], ref1_[:, 2], "g.", ms=6, alpha=0.6)
+ax0.plot(ref2_[:, 0], ref2_[:, 1], ref2_[:, 2], "g.", ms=6, alpha=0.6)
 ax0.plot(pareto_front[:, 0], pareto_front[:, 1], pareto_front[:, 2], "k.", mec="none", ms=5, alpha=0.4)
 
 ax0.set_title("Initialization")
@@ -145,4 +178,4 @@ ax1.set_ylabel(r"$f_3$")
 
 plt.tight_layout()
 # plt.show()
-plt.savefig(f"MMD-{f.__class__.__name__}.pdf", dpi=1000)
+plt.savefig(f"MMD-{f.__class__.__name__}_gap.pdf", dpi=1000)
