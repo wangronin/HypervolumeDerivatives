bfeea3994a075aee091a775b9c2ccfe7e97cdde5
wangronin <wangronin@gmail.com>
2026-05-19 01:22:59 +0200

    update

--- examples/DpN/conv3_degenerate_example.py
+++ examples/DpN/conv3_degenerate_example.py
@@ -7,8 +7,10 @@ import numpy as np
 import pandas as pd
 from matplotlib import rcParams
 
-from hvd.newton import HVN
 from hvd.problems import CONV3
+from hvd.delta_p import GenerationalDistance, InvertedGenerationalDistance
+from hvd.newton import DpN
+from hvd.reference_set import ReferenceSet
 
 np.random.seed(42)
 np.set_printoptions(edgeitems=30, linewidth=100000)
@@ -28,36 +30,39 @@ rcParams["ytick.major.width"] = 1
 np.random.seed(66)
 
 f = CONV3()
-max_iters = 22
+max_iters = 6
 pareto_front = f.get_pareto_front(N=10000)
-x0 = pd.read_csv("./data/CONV3_X0.csv", header=None).values
+ref = pd.read_csv("./data/CONV3_linear_reference.csv", header=None).values
+x0 = pd.read_csv("./data/CONV3_X0_line_no_rep.csv", header=None).values
 y0 = np.array([f.objective(x) for x in x0])
 N = len(x0)
-ref = np.array([30, 30, 30])
-opt = HVN(
-    n_var=3,
+
+metrics = dict(GD=GenerationalDistance(ref=ref), IGD=InvertedGenerationalDistance(ref=ref))
+opt = DpN(
+    dim=3,
     n_obj=3,
-    ref=ref,
+    ref=ReferenceSet(ref=ref),
     func=f.objective,
     jac=f.objective_jacobian,
     hessian=f.objective_hessian,
     N=N,
+    x0=x0,
     xl=-3,
     xu=3,
-    X0=x0,
     max_iters=max_iters,
     verbose=True,
     preconditioning=True,
-    limit_max_step_size=True,
+    metrics=metrics,
+    type="igd",
 )
 X, Y, stop = opt.run()
 # plotting
+reference_set = opt.ref.reference_set
 x, y, z = pareto_front[:, 0], pareto_front[:, 1], pareto_front[:, 2]
 triang = mtri.Triangulation(x, y)
 xmid = x[triang.triangles].mean(axis=1)
 ymid = y[triang.triangles].mean(axis=1)
 zmid = z[triang.triangles].mean(axis=1)
-
 p = np.c_[xmid, ymid, zmid]
 mask = np.array([np.any(np.all(pp > p, axis=1)) for pp in p])
 triang.set_mask(mask)
@@ -65,11 +70,12 @@ triang.set_mask(mask)
 fig = plt.figure(figsize=(20, 8))
 ax = fig.add_subplot(1, 3, 1, projection="3d")
 ax.set_box_aspect((1, 1, 1))
-ax.view_init(15, 45)
+ax.view_init(-45, 40)
 lines = []
 lines += ax.plot(y0[:, 0], y0[:, 1], y0[:, 2], "k.", ms=13)
+lines += ax.plot(reference_set[:, 0], reference_set[:, 1], reference_set[:, 2], "g+", ms=13)
 lines.append(ax.plot_trisurf(triang, z, color="k", alpha=0.2))
-ax.legend(lines, [r"$Y_0$", r"Pareto front"], loc="lower right")
+ax.legend(lines, [r"$Y_0$", "reference set", r"Pareto front"], loc="lower right")
 ax.set_title("objective space")
 ax.set_xlabel(r"$f_1$")
 ax.set_ylabel(r"$f_2$")
@@ -77,11 +83,13 @@ ax.set_zlabel(r"$f_3$")
 
 ax = fig.add_subplot(1, 3, 2, projection="3d")
 ax.set_box_aspect((1, 1, 1))
-ax.view_init(15, 45)
+ax.view_init(-45, 40)
+
 lines = []
 lines += ax.plot(Y[:, 0], Y[:, 1], Y[:, 2], "r.", ms=13)
+lines += ax.plot(reference_set[:, 0], reference_set[:, 1], reference_set[:, 2], "g+", ms=13)
 lines.append(ax.plot_trisurf(triang, z, color="k", alpha=0.2))
-ax.legend(lines, [r"$Y_{\text{final}}$", r"Pareto front"], loc="lower right")
+ax.legend(lines, [r"$Y_{\text{final}}$", "reference set", r"Pareto front"], loc="lower right")
 ax.set_title("objective space")
 ax.set_xlabel(r"$f_1$")
 ax.set_ylabel(r"$f_2$")
@@ -95,7 +103,7 @@ ax.semilogy(range(1, len(opt.history_indicator_value) + 1), opt.history_indicato
 ax_.semilogy(range(1, len(opt.history_R_norm) + 1), opt.history_R_norm, "g--")
 ax.set_ylabel("")
 ax_.set_ylabel("")
-ax.text(0, 1.01, r"$\operatorname{HV}$", transform=ax.transAxes, color="b", ha="right", va="bottom")
+ax.text(0, 1.01, r"$\Delta_p$", transform=ax.transAxes, color="b", ha="right", va="bottom")
 ax_.text(
     1.12, 1.01, r"$||R_I(\mathbf{X}, \lambda)||$", transform=ax.transAxes, color="g", ha="right", va="bottom"
 )
@@ -104,4 +112,4 @@ ax.set_xlabel("iteration")
 
 plt.tight_layout()
 plt.subplots_adjust(wspace=0.15)
-plt.savefig(f"CONV3-HVN-example-{N}.pdf", dpi=100)
+plt.savefig(f"CONV3-DpN-degenerate-example-{N}.pdf", dpi=100)
